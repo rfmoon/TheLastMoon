b4644c8c5c99cae8b2afcaa7412bd4b2a1dcd9a5
rfmoon <hantumana13@gmail.com>
2026-08-13 10:05:11 +0700

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -15,7 +15,7 @@ const MENUS = Object.freeze([
   { id: "user-admin", label: "User Admin", icon: "♙", masterOnly: true }
 ]);
 
-const VERSION = "v20-checker-full-replace";
+const VERSION = "v21-checker-strict-match";
 const COOKIE_NAME = "thelastmoon_session";
 const SESSION_TTL_MS = 12 * 60 * 60 * 1000;
 const PASSWORD_ITERATIONS = 60000;
