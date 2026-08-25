f04bb65ef3e450d81697f87996bc42a0feaa245a
rfmoon <hantumana13@gmail.com>
2026-08-25 11:45:22 +0700

    Add files via upload

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -16,7 +16,7 @@ const MENUS = Object.freeze([
   { id: "user-admin", label: "User Admin", icon: "♙", masterOnly: true }
 ]);
 
-const VERSION = "v41-wd-bersih-shared-realtime";
+const VERSION = "v42-excel-match-contoh";
 const COOKIE_NAME = "thelastmoon_session";
 const SESSION_TTL_MS = 12 * 60 * 60 * 1000;
 const PASSWORD_ITERATIONS = 60000;
