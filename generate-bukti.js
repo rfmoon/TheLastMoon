6192666cfbc4dc5055bf86b081d1b78941a43502
rfmoon <hantumana13@gmail.com>
2026-08-29 09:57:43 +0700

    Add files via upload

--- generate-bukti.js
+++ generate-bukti.js
@@ -1,10 +1,12 @@
 const type=document.getElementById("gbType");
 const frame=document.getElementById("gbFrame");
+
 const pages={
-"antar-bank":"/generate-bukti-antar-bank.html?v=52.0.0",
-"sesama-bca":"/generate-bukti-sesama-bca.html?v=52.0.0"
+  "antar-bank":"/generate-bukti-antar-bank.html?v=52.0.0",
+  "sesama-bca":"/generate-bukti-sesama-bca.html?v=52.0.0"
 };
+
 type.addEventListener("change",()=>{
-const next=pages[type.value];
-if(next) frame.src=next;
+  const next=pages[type.value];
+  if(next) frame.src=next;
 });
