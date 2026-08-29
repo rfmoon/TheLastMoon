80724db26cb66c341c211cc271a14cfa9c8d46e2
rfmoon <hantumana13@gmail.com>
2026-08-29 09:39:38 +0700

    Add files via upload

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -18,7 +18,7 @@ const MENUS = Object.freeze([
   { id: "user-admin", label: "User Admin", icon: "♙", masterOnly: true }
 ]);
 
-const VERSION = "v51-generate-bukti-bca-demo";
+const VERSION = "v52-generate-bukti-original-scripts";
 const COOKIE_NAME = "thelastmoon_session";
 const SESSION_TTL_MS = 12 * 60 * 60 * 1000;
 const PASSWORD_ITERATIONS = 60000;
